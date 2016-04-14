178638e49f6354d3c9405d90db687c12cfc0e0f6
Jake Snell <jsnell@cs.toronto.edu>
2016-04-14 15:56:44 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='myshkin',
+      version='0.0.1',
+      description='Utilities for training deep nets in TensorFlow',
+      author='Jake Snell',
+      author_email='jsnell10@gmail.com',
+      packages=['myshkin'])
