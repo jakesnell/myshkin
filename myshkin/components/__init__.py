bbd5c7a44a1bb03a0b54d16702eb8333c8dbe902
Jake Snell <jsnell@cs.toronto.edu>
2016-04-28 08:08:23 -0400

    remove reshape op

--- myshkin/components/__init__.py
+++ myshkin/components/__init__.py
@@ -1,5 +1,4 @@
 from conv import MaxPool2D
 from image import ResizeNearestNeighbor
 from nonlinearities import Identity, Relu, Sigmoid
-from reshape import Reshape
 from sequential import Sequential
