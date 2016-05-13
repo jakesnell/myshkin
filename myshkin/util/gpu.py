11608b97bfcc038d0f96a14599360ddbf5b7f0d1
Jake Snell <jsnell@cs.toronto.edu>
2016-05-13 14:10:17 -0400

    add timeout to board

--- myshkin/util/gpu.py
+++ myshkin/util/gpu.py
@@ -1,10 +1,15 @@
 # get board
-import os
+import os, time
 import gpu_lock2 as gg
 
 def get_board():
     board_id = gg.obtain_lock_id()
     if board_id < 0:
-        raise Exception("could not obtain lock")
+        time.sleep(10)
+        board_id = gg.obtain_lock_id() # try again
+        if board_id < 0:
+            raise Exception("could not obtain lock")
+        else:
+            return board_id
     else:
         return board_id
